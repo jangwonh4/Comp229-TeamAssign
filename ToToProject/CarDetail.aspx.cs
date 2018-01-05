a92085ff4a5edab0699b1a8150a15e9a9e737bbb
Soohwanlee <32316390+Soohwanlee@users.noreply.github.com>
2018-01-05 18:19:13 -0500

    create a function of booking

--- ToToProject/CarDetail.aspx.cs
+++ ToToProject/CarDetail.aspx.cs
@@ -31,6 +31,7 @@ namespace ToToProject
         private void DisplayData()
         {
             int CarID = Convert.ToInt32(Request.QueryString["Name"]);
+
             SqlCommand comm = new SqlCommand("SELECT * FROM Cars where CarID = @CarID", conn);
             comm.Parameters.Add("@CarID", System.Data.SqlDbType.Int);
             comm.Parameters["@CarID"].Value = CarID;
@@ -46,17 +47,39 @@ namespace ToToProject
         // Rent a Car button
         protected void Rent_Click(object sender, EventArgs e)
         {
+            int CarID = Convert.ToInt32(Request.QueryString["Name"]);
+
+            SqlCommand comm = new SqlCommand("SELECT * FROM Cars where CarID = @CarID", conn);
+            comm.Parameters.Add("@CarID", System.Data.SqlDbType.Int);
+            comm.Parameters["@CarID"].Value = CarID;
+            conn.Open();
+            SqlDataReader reader = comm.ExecuteReader();
+            GridView2.DataSource = reader;
+            GridView2.DataBind();
+            reader.Close();
+            conn.Close();
+
+
             String DateS = null;
             String DateE = null;
-            SqlCommand RentACar = new SqlCommand("INSERT INTO CarLine(DateStart, DateEnd) VALUES(@dateS, @dateE)", conn);
+            String UserName = Convert.ToString(Session["User"]);
+            SqlCommand RentACar = new SqlCommand(
+                "INSERT INTO CarLine(DateStart, DateEnd, CustomerID,CarID) VALUES(@dateS, @dateE,(select CustomerID from dbo.Customer where Username=@UserName),@CarID)"
+                , conn);
+
 
             //select date in the tables
+
             DateS = Convert.ToString(Calendar1.SelectedDate);
             DateE = Convert.ToString(Calendar2.SelectedDate);
             try
             {
                 RentACar.Parameters.AddWithValue("@dateS", DateS);
                 RentACar.Parameters.AddWithValue("@dateE", DateE);
+                RentACar.Parameters.Add("@CarID", System.Data.SqlDbType.Int);
+                RentACar.Parameters.Add("@UserName", System.Data.SqlDbType.VarChar);
+                RentACar.Parameters["@CarID"].Value = CarID;
+                RentACar.Parameters["@UserName"].Value = UserName;
                 conn.Open();
                 RentACar.ExecuteNonQuery();
                 Response.Redirect(Request.RawUrl);
